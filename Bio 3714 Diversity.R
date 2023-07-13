16da28ce2e6fb4fd03310114d58e265e109c4ed2
Ranjan Wagle <115357615+ranjanwagle82@users.noreply.github.com>
2023-07-13 19:29:23 -0230

    Add files via upload
    # Field Course Data Analysis Code
    
    ## Introduction
    
    This code, written by Ranjan Wagle, was developed as part of a teaching assistant's work during a field course at Bonney Bay. The purpose of this code is to perform data analysis on catch numbers and collection tracking data from the year 2018.
    
    The code utilizes the R programming language and several libraries, including `readxl` for reading Excel files and `dplyr` for data manipulation and analysis.
    
    ## Code Overview
    
    The code performs the following main steps:
    
    1. Reads the catch numbers and collection tracking data for the year 2018 from Excel files.
    2. Filters and selects relevant columns from the catch numbers and collection tracking datasets.
    3. Merges the filtered data from both datasets based on common columns.
    4. Calculates diversity metrics and counts unique values for each site and gear type.
    5. Calculates the H' diversity metric based on the total number of catches.
    6. Joins the diversity metrics with the count data.
    7. Writes comments in each code section to provide comprehensive explanations of the code's purpose.
    
    ## Usage
    
    To use this code, ensure that the necessary Excel files are available in the specified directory. Adjust the file paths if needed. The code can be executed in an R environment or an R script.

---
+++ Bio 3714 Diversity.R
@@ -0,0 +1,59 @@
+library(readxl)
+library(dplyr)
+
+# Read Catch Numbers and Collection Tracking data for the year 2018
+Catch_Numbers_2018 <- read_excel("Downloads/data for calculation/Catch Numbers 2018-2022.xlsx", sheet = "CN")
+Collection_2018 <- read_excel("Downloads/data for calculation/Collection Tracking 2018-2022.xlsx", sheet = "CT")
+
+# Filter and select relevant columns from Catch Numbers data
+Firstdata <- Catch_Numbers_2018 %>%
+  filter(`Gear type` %in% c("10 m beach seine", "25 m beach seine")) %>% 
+  group_by(`Gear type`, `Site name`) %>%
+  select(Year, `Gear type`, `Site name`, `Common name`, `Number caught`, `Unique Tow`)
+
+# Filter and select relevant columns from Collection Tracking data
+Collection_Firstdata <- Collection_2018 %>%
+  filter(`Gear type` %in% c("10 m beach seine", "25 m beach seine")) %>% 
+  group_by(`Gear type`, `Site name`) %>%
+  select(Year, `Gear type`, `Site name`, `Unique Tow`, `Fish caught?`)
+
+# Merge the filtered data from Catch Numbers and Collection Tracking
+merge_data <- merge(Firstdata, Collection_Firstdata, by = c("Year", "Gear type", "Site name", "Unique Tow"), all.x = TRUE, all.y = TRUE)
+
+# Count unique values and calculate diversity metrics
+count <- merge_data %>%
+  group_by(`Site name`, `Gear type`) %>%
+  summarize(`S` = n_distinct(na.omit(`Common name`)), 
+            `No. of tow` = n_distinct(na.omit(`Unique Tow`)),
+            `S'` = (S / `No. of tow`),
+            `Common names` = toString(unique(na.omit(`Common name`)))), 
+.groups = 'drop')
+
+# Calculate H' diversity metric
+H_diversity_summary <- Catch_Numbers_2018 %>%
+  filter(`Gear type` %in% c("10 m beach seine", "25 m beach seine")) %>% 
+  group_by(`Gear type`, `Site name`, `Common name`) %>%
+  summarize(`Total Number Caught` = sum(`Number caught`)) %>%
+  ungroup() %>%
+  group_by(`Gear type`, `Site name`) %>%
+  mutate(`Pi` = `Total Number Caught` / sum(`Total Number Caught`),
+         Pi_ln = Pi * log(Pi)) %>%
+  group_by(`Gear type`, `Site name`) %>%
+  summarize(`H'` = (-1) * sum(Pi_ln))
+
+# Join the diversity metrics with the count data
+Final_data <- H_diversity_summary %>%
+  left_join(count, by = c("Site name", "Gear type"))
+
+
+
+
+
+
+
+
+
+
+
+
+
